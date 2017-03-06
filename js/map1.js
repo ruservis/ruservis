91ddf97a825319fd12213f4b38de64d7ed54e39b
vk-servis <brewbaker@yandex.ru>
2017-03-06 05:47:58 +0300

    Update map1.js

--- js/map1.js
+++ js/map1.js
@@ -171,3 +171,24 @@ ymaps.ready(function () {
         currentRoute = currentRoutingMode = null;
     }
 });
+
+ // Макет кнопки должен отображать поле data.content
+        // и изменяться в зависимости от того, нажата кнопка или нет.
+        ButtonLayout = ymaps.templateLayoutFactory.createClass(
+            "<div class='my-button [if state.selected]my-button-selected[endif]'>" +
+                "$[data.content]" +
+                "</div>"
+        ),
+            
+        button = new ymaps.control.Button({
+            data: {
+                content: "Жмак-жмак"
+            }
+        }, {
+            layout: ButtonLayout
+        });
+            
+    myMap.controls.add(button, {
+        right: 5,
+        top: 5
+    });
